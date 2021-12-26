8d45fe91746a04c48e12a3f304f0b6386ab9fd83
Matiss <matiss.zemturis@gmail.com>
2021-12-26 11:39:17 +0200

    defined class for single record

---
+++ flutter_form/lib/Classes/record.dart
@@ -0,0 +1,15 @@
+class Record {
+  int rating;
+  String date;
+  String distractions;
+  String positive;
+  String note;
+
+  Record({
+    required this.rating,
+    required this.date,
+    required this.distractions,
+    required this.positive,
+    required this.note,
+  });
+}
